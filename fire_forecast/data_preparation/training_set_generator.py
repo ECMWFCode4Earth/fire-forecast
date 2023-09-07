2ed8b979b317e845078c7e0e72f76d53f83b7af5
christopher-lw <chlw@your.hostname.com>
2023-09-07 07:42:33 +0000

    Add dask compatibility and sequential saving.

--- fire_forecast/data_preparation/training_set_generator.py
+++ fire_forecast/data_preparation/training_set_generator.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 from typing import List, Tuple
 
+import dask.array as da
 import h5py
 import numpy as np
 import xarray as xr
+from loguru import logger
+from tqdm.auto import tqdm
+
+CHUNK_SIZE = int(1e5)
 
 
 class TrainingSetGenerator:
@@ -47,11 +52,12 @@ class TrainingSetGenerator:
         variable_selection.extend(meteo_variables.copy())
         dataset = self.dataset[variable_selection]
         # for satiellite_index in dataset.ident.values:
+        logger.info("Applying first day selection...")
         satellite_dataset = dataset.where(
             (
                 dataset.frpfire.isel(
                     longitude_pixel=1, latitude_pixel=1, time_index=slice(0, 24)
-                )
+                ).compute()
                 > 0
             ).sum("time_index")
             >= fire_threshold_first_day,
@@ -61,17 +67,18 @@ class TrainingSetGenerator:
             (
                 satellite_dataset.offire.isel(
                     longitude_pixel=1, latitude_pixel=1, time_index=slice(0, 24)
-                )
+                ).compute()
                 > 0
             ).sum("time_index")
             >= measurement_threshold_first_day,
             drop=True,
         )
+        logger.info("Applying second day selection...")
         satellite_dataset = satellite_dataset.where(
             (
                 satellite_dataset.frpfire.isel(
                     longitude_pixel=1, latitude_pixel=1, time_index=slice(24, 48)
-                )
+                ).compute()
                 > 0
             ).sum("time_index")
             >= fire_threshold_second_day,
@@ -81,7 +88,7 @@ class TrainingSetGenerator:
             (
                 satellite_dataset.offire.isel(
                     longitude_pixel=1, latitude_pixel=1, time_index=slice(24, 48)
-                )
+                ).compute()
                 > 0
             ).sum("time_index")
             >= measurement_threshold_second_day,
@@ -89,35 +96,41 @@ class TrainingSetGenerator:
         )
         # filter out nan values
         if filter_nans:
+            logger.info("Filtering nans...")
             # filter out nan values
-            for data_var in satellite_dataset.data_vars:
+            for data_var in tqdm(satellite_dataset.data_vars, desc="Filtering nans"):
                 data = satellite_dataset[data_var]
                 if "ident" in data.dims:
                     satellite_dataset = satellite_dataset.where(
-                        (~np.isnan(data)).prod(
+                        (~np.isnan(data))
+                        .prod(
                             [
                                 "longitude_pixel",
                                 "latitude_pixel",
                                 "time_index",
                                 "ident",
                             ]
-                        ),
+                        )
+                        .compute(),
                         drop=True,
                     )
                 else:
                     satellite_dataset = satellite_dataset.where(
-                        (~np.isnan(data)).prod(
-                            ["longitude_pixel", "latitude_pixel", "time_index"]
-                        ),
+                        (~np.isnan(data))
+                        .prod(["longitude_pixel", "latitude_pixel", "time_index"])
+                        .compute(),
                         drop=True,
                     )
-
+        logger.info("Done with filtering")
         # satellite_subsets.append(satellite_dataset)
         dataset = satellite_dataset  # xr.concat(satellite_subsets, dim="sample")
         dataarrays = []
-        for data_variable in variable_selection:
-            dataarrays.append(dataset[data_variable].values)
-        return np.array(dataarrays).transpose(1, 0, 2, 3, 4), variable_selection
+        for data_variable in tqdm(variable_selection, desc="Selecting variables"):
+            dataarrays.append(dataset[data_variable].data)
+        return (
+            da.stack(dataarrays, axis=0).transpose(1, 0, 2, 3, 4),
+            variable_selection,
+        )
 
     def save_training_set(
         self,
@@ -154,8 +167,33 @@ class TrainingSetGenerator:
             fire_threshold_second_day,
             measurement_threshold_second_day,
         )
+        logger.info(f"Opening h5 file {output_path}")
         with h5py.File(output_path, "w") as f:
-            f.create_dataset("training_set", data=training_set)
+            training_sample_shape = training_set.shape[1:]
+            dataset = f.create_dataset(
+                "training_set",
+                shape=(0, *training_sample_shape),
+                maxshape=(None, *training_sample_shape),
+                dtype=np.float32,
+            )
+            logger.info("Start saving dataset chunks:")
+            chunk_index = 0
+            while True:
+                upper_bound = min((chunk_index + 1) * CHUNK_SIZE, training_set.shape[0])
+                chunk = training_set[chunk_index * CHUNK_SIZE : upper_bound]  # noqa
+                if chunk.shape[0] == 0:
+                    break
+                logger.info(f"Saving chunk {chunk_index}. Chunk shape: {chunk.shape}")
+                dataset.resize((dataset.shape[0] + chunk.shape[0], *dataset.shape[1:]))
+                try:
+                    dataset[-chunk.shape[0] :] = chunk.compute()  # noqa
+                    logger.info("Chunk saved with dask.")
+                except AttributeError:
+                    dataset[-chunk.shape[0] :] = chunk  # noqa
+                    logger.info("Chunk saved without dask.")
+
+                chunk_index += 1
+
             f.create_dataset(
                 "variable_selection", data=np.array(variable_selection, dtype="S")
             )
